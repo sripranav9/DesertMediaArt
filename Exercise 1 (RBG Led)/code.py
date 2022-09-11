7f095902c47f73e5110e4248ea2968a8e41bab30
Sri Pranav Srivatsavai <92122776+sripranav9@users.noreply.github.com>
2022-09-11 15:30:51 +0400

    added more scenes

--- Exercise 1 (RBG Led)/code.py
+++ Exercise 1 (RBG Led)/code.py
@@ -41,16 +41,15 @@ while brightness <= 0.35:
 
 # scene - pleasant
 i = 0
-while i < 6000:
+while i < 8000:
     led.brightness = brightness
     brightness = (brightness+0.001) % 1
     led[0] = (255,255,0)
     i+=1
 
-
 #scene - panzar enters
 j = 0
-while j < 4:
+while j < 0.25:
     led.brightness = 1
     led[0] = (255,0,0)
     time.sleep(1)
@@ -58,5 +57,48 @@ while j < 4:
     time.sleep(1)
     led.brightness = 1
     time.sleep(1)
+    j+=0.1
+
+# scene - shooting begins
+k = 0;
+while k < 1.3:
+    led.brightness = 0.5
+    led[0] = (255,0,0)
+    time.sleep(0.25)
+    led[0] = (0,0,255)
+    time.sleep(0.25)
+
+    led.brightness = 1
+    led[0] = (255,0,0)
+    time.sleep(0.25)
+    led[0] = (0,0,255)
+    time.sleep(0.25)
+
+    k+=0.1
+
+# scene - stops shooting - rainbow setting
+i = 0
+a = 0
+while a<1000:
+    i = (i + 1) % 256  # run from 0 to 255
+    led.fill(colorwheel(i))
+    time.sleep(0.01)
+    a+=1
 
 
+# scene - yellow lights "put him in the background"
+w = 0
+while w < 3750:
+    led.brightness = brightness
+    brightness = (brightness+0.001) % 1
+    led[0] = (255,255,0)
+    w+=1
+
+# scene - focus on yoghurt
+i = 0
+a = 0
+while a<500:
+    i = (i + 1) % 256  # run from 0 to 255
+    led.fill(colorwheel(i))
+    time.sleep(0.01)
+    a+=1
