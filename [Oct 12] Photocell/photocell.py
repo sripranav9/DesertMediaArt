0ced09c8c5c88d26f12e6b204b81fd5026a1603d
Sri Pranav Srivatsavai <92122776+sripranav9@users.noreply.github.com>
2022-10-12 12:08:52 +0400

    Create photocell.py
    code from adafruit, modified by mang, pranav for the class

---
+++ [Oct 12] Photocell/photocell.py
@@ -0,0 +1,30 @@
+# 12 October 2022, In-class Exercise
+
+# SPDX-FileCopyrightText: 2018 Anne Barela for Adafruit Industries
+# SPDX-License-Identifier: MIT
+# Source: https://learn.adafruit.com/make-it-change-potentiometers/circuitpython
+
+# Modified by Sri Pranav Srivatsavai
+# Under the guidance of Professor mang
+# Desert Media Art – Photocell and Potentiometer
+
+import time
+import board
+from analogio import AnalogIn
+
+photoCell = AnalogIn(board.A1)  # potentiometer connected to A1, power & ground
+
+darkThreshold = 28000
+brightThreshold = 45000
+
+while True:
+
+    brightness = photoCell∂.value
+    print((brightness,))      # Display value
+    
+    if (brightness > brightThreshold):
+        print('bright')
+    if (brightness < darkThreshold):
+        print('dark')
+
+    time.sleep(0.25)                   # Wait a bit before checking the value again
